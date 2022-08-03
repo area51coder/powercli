8dbad8561e892ad0a4ee653670b7e87f9460110a
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-03 22:30:58 +0530

    Update hello.ps1

--- file/hello.ps1
+++ file/hello.ps1
@@ -3,7 +3,8 @@
 Write-Host
 Write-Host 'Hello World!'
 Write-Host "Good-bye World! `n"
-Set-PowerCLIConfiguration -InvalidCertificateAction > Ignore -Scope AllUsers*
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Scope AllUsers
+#Set-PowerCLIConfiguration -InvalidCertificateAction > Ignore -Scope AllUsers*
 #Find-Module "VMware.PowerCLI" | Install-Module -Scope "CurrentUser" -AllowClobber
 #pwsh -command Connect-VIServer 192.168.29.195 -User Administrator@vsphere.local -Password B@ond007
 # end of script
