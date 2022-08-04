531ac10acf2def06e4f0e6c95507fb00f9f140d5
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-04 15:04:35 +0530

    Update vminfo.ps1

--- vminfo.ps1
+++ vminfo.ps1
@@ -22,4 +22,4 @@ $VmInfo = ForEach ($Datacenter in (Get-Datacenter | Sort-Object -Property Name))
     }
   }
 }
-$VmInfo | Export-Csv -NoTypeInformation -UseCulture -Path "file/VmInfo.csv"
+$VmInfo | Export-Csv -NoTypeInformation -UseCulture -Path "VmInfo.csv"
