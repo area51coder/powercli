097a4982434a9995b77f1796813b79aa3a44b75b
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-04 19:45:15 +0530

    Update vminfo.ps1

--- vminfo.ps1
+++ vminfo.ps1
@@ -8,21 +8,4 @@
 pwsh -NoExit -Command Connect-VIServer 192.168.29.195 -User Administrator@vsphere.local -Password B@ond007
 pwsh -Command Start-Sleep -Seconds 30
 #pwsh -NoExit -Command Connect-VIServer 192.168.29.195 -User Administrator@vsphere.local -Password B@ond007
-$VmInfo = ForEach ($Datacenter in (Get-Datacenter | Sort-Object -Property Name)) {
-  ForEach ($Cluster in ($Datacenter | Get-Cluster | Sort-Object -Property Name)) {
-    ForEach ($VM in ($Cluster | Get-VM | Sort-Object -Property Name)) {
-      ForEach ($HardDisk in ($VM | Get-HardDisk | Sort-Object -Property Name)) {
-        "" | Select-Object -Property @{N="VM";E={$VM.Name}},
-          @{N="Datacenter";E={$Datacenter.name}},
-          @{N="Cluster";E={$Cluster.Name}},
-          @{N="Hard Disk";E={$HardDisk.Name}},
-          @{N="Datastore";E={$HardDisk.FileName.Split("]")[0].TrimStart("[")}},
-          @{N="VMConfigFile";E={$VM.ExtensionData.Config.Files.VmPathName}},
-          @{N="VMDKpath";E={$HardDisk.FileName}},
-                  @{N="Storage Format";E={$HardDisk.StorageFormat}},
-          @{N="VMDK Size";E={($vm.extensiondata.layoutex.file|?{$_.name -contains $harddisk.filename.replace(".","-flat.")}).size/1GB}}
-      }
-    }
-  }
-}
-$VmInfo | Export-Csv -NoTypeInformation -UseCulture -Path "VmInfo.csv"
+vminfocollect.ps1
