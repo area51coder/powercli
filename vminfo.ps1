dd407c69e02e857d32dc0dc0eb717b9233aa85a0
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-04 21:44:40 +0530

    Update vminfo.ps1

--- vminfo.ps1
+++ vminfo.ps1
@@ -6,6 +6,6 @@
 #$Credential.Password | ConvertFrom-SecureString | Set-Content B@ond007
 #Connect-VIServer $Server -Credential $Credential
 pwsh -NoExit -Command Connect-VIServer 192.168.29.195 -User Administrator@vsphere.local -Password B@ond007
-pwsh -Command Start-Sleep -Seconds 30
+#pwsh -Command Start-Sleep -Seconds 30
 #pwsh -NoExit -Command Connect-VIServer 192.168.29.195 -User Administrator@vsphere.local -Password B@ond007
-invoke-expression -Command vminfocollect.ps1
+./vminfocollect.ps1
