893a95dfdac87a54a1d6c5fe0d6c8307e9bc64ff
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-04 10:39:35 +0530

    Update vminfo.ps1

--- vminfo.ps1
+++ vminfo.ps1
@@ -1,5 +1,11 @@
 #!/usr/bin/pwsh -Command
 write-host -fore Green "executing PowerShell!";
+set-psrepository -name "PSGallery" -InstallationPolicy Trusted
+Install-Module -Name VMware.PowerCLI -Scope CurrentUser
+import-module vmware.vimautomation.core
+import-module vmware.vimautomation.sdk
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 #$Server = 192.168.29.195
 #$Credential = Get-Credential Administrator@vsphere.local
 #$Credential.Password | ConvertFrom-SecureString | Set-Content B@ond007
