2578aa312c77b28790abb419c25a2c134ff4a1d9
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-05 11:11:03 +0530

    Update vminfo.ps1

--- vminfo.ps1
+++ vminfo.ps1
@@ -5,9 +5,9 @@
 #$Credential = Get-Credential Administrator@vsphere.local
 #$Credential.Password | ConvertFrom-SecureString | Set-Content B@ond007
 #Connect-VIServer $Server -Credential $Credential
-pwsh -NoExit -Command Connect-VIServer 192.168.29.195 -User Administrator@vsphere.local -Password B@ond007
-#pwsh -Command Start-Sleep -Seconds 30
 #pwsh -NoExit -Command Connect-VIServer 192.168.29.195 -User Administrator@vsphere.local -Password B@ond007
+#pwsh -Command Start-Sleep -Seconds 30
+Connect-VIServer 192.168.29.195 -User Administrator@vsphere.local -Password B@ond007
 #.\vminfocollect.ps1
 $VmInfo = ForEach ($Datacenter in (Get-Datacenter | Sort-Object -Property Name)) {
   ForEach ($Cluster in ($Datacenter | Get-Cluster | Sort-Object -Property Name)) {
